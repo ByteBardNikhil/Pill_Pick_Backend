62002e6585a8ae4f03452bf90eee0dcdf227be3c
Nikhil-197 <mahajannikhil177@gmail.com>
2024-03-28 22:38:59 +0530

    Initial

---
+++ src/main/java/com/backend/Pill_Pick/PillPickApplication.java
@@ -0,0 +1,13 @@
+package com.backend.Pill_Pick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillPickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PillPickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/Pill_Pick/PillPickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.Pill_Pick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillPickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
