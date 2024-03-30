f77364b8dbf17a89c59469fedbc930040a03786d
Nikhil-197 <mahajannikhil177@gmail.com>
2024-03-31 02:11:10 +0530

    Starting login

--- src/main/java/com/backend/Pill_Pick/PillPickApplication.java
+++ src/main/java/com/backend/Pill_Pick/PillPickApplication.java
@@ -2,8 +2,9 @@ package com.backend.Pill_Pick;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class PillPickApplication {
 
 	public static void main(String[] args) {
